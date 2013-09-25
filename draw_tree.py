78d7e7c2dd04b22eb9ca0c9c5d27ebfca69f5fc4
bendmorris <ben@bendmorris.com>
2013-09-26 08:18:54 +0930

    Initial commit.

---
+++ draw_tree.py
@@ -0,0 +1,17 @@
+# USAGE:
+#   python draw_tree.py [tree file] [output image file]
+import sys
+import Bio.Phylo as bp
+import matplotlib.pyplot as plt
+
+# get command line arguments
+tree_file = sys.argv[1]
+output_image = sys.argv[2]
+
+# read in the input tree
+tree = bp.read(tree_file, 'newick')
+tree.root_at_midpoint()
+
+# draw the tree
+bp._utils.draw(tree, do_show=False)
+plt.savefig(output_image)
